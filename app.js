f06e49291a75ec73cea40d067477aa84ea262ec8
Youlink <younes.rahmani@hotmail.fr>
2023-10-06 17:46:43 +0200

    calculette OK

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-/* */
+console.log("hello");
+const touches = [...document.querySelectorAll('.bouton')];
+const listeKeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('.result');
+
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyCode.toString();
+    calculer(valeur)
+})
+
+
+document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
+})
+
+const calculer = (valeur) => {
+    if (listeKeycode.includes(valeur)) {
+        switch (valeur) {
+            case '27':
+                ecran.textContent = "";
+                break;
+            case '187':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+            default:
+                const indexKeycode = listeKeycode.indexOf(valeur);
+                const touche = touches[indexKeycode];
+                ecran.textContent += touche.innerHTML;
+        }
+    }
+}
+
+window.addEventListener('error', (e) => {
+    alert('une erreur est survenue dans votre calcul:' + e.message)
+})
